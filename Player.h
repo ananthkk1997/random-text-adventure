3a3801f6f524009354b13e2946fa08c3ca7aedcc
Christian Nolan Go <christiannolango@gmail.com>
2017-01-29 14:48:46 -0700

    Add files via upload

---
+++ Player.h
@@ -0,0 +1,14 @@
+//
+// Created by Christian on 1/29/2017.
+//
+
+#ifndef RANDOM_TEXT_ADVENTURE_PLAYER_H
+#define RANDOM_TEXT_ADVENTURE_PLAYER_H
+
+
+class Player {
+
+};
+
+
+#endif //RANDOM_TEXT_ADVENTURE_PLAYER_H
