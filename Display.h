41dfe337cb8b763101c2732d26ae3dff8d1c180e
Christian Nolan Go <chrisitannolango@gmail.com>
2017-01-30 11:48:07 -0700

    Rudimentary Player Class

---
+++ Display.h
@@ -0,0 +1,14 @@
+//
+// Created by Christian on 1/30/2017.
+//
+
+#ifndef RANDOM_TEXT_ADVENTURE_DISPLAY_H
+#define RANDOM_TEXT_ADVENTURE_DISPLAY_H
+
+
+class Display {
+
+};
+
+
+#endif //RANDOM_TEXT_ADVENTURE_DISPLAY_H
